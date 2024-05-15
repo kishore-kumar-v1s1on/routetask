59a4140db6b7ccdb5e3ebf841da54d683782b602
Gopi <GopiR5@yahoo.com>
2024-05-15 22:09:52 +0530

    fsds

--- src/components/Blockchain.jsx
+++ src/components/Blockchain.jsx
@@ -6,7 +6,7 @@ export default function Blockchain() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5173/blockchain.json")
+    fetch("https://6644d1124021401d276ce071--transcendent-capybara-5580d6.netlify.app/blockchain.json")
       .then((response) => response.json())
       .then((result) => setProducts(result.data))
       .catch((error) => console.log(error));
