2106cb6f074f5afedd5bb70ef44e2e9be9224df8
Gopi <GopiR5@yahoo.com>
2024-05-16 05:16:27 +0530

    ch adds dets

--- src/components/Blockchain.jsx
+++ src/components/Blockchain.jsx
@@ -6,7 +6,7 @@ export default function Blockchain() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5173/blockchain.json")
+    fetch("https://664547779b173c0008c7d331--gentle-bavarois-6f6b67.netlify.app/blockchain.json")
       .then((response) => response.json())
       .then((result) => setProducts(result.data))
       .catch((error) => console.log(error));
